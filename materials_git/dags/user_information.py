4d210585af48a633ebfe2fad0a41007b901af5f2
AmanT08 <amanthapliyal182001@gmail.com>
2025-05-21 18:30:47 +0530

    added task to store the processed data, used hook

--- materials_git/dags/user_information.py
+++ materials_git/dags/user_information.py
@@ -1,12 +1,27 @@
 from airflow import DAG
 from datetime import datetime
-
+import os
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 import json
 from pandas import json_normalize
+
+
+def _store_user():
+    hook = PostgresHook(postgres_conn_id = 'postgres')
+
+    file_path = '/tmp/processed_user.csv'
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"The file {file_path} does not exist.")
+
+
+    hook.copy_expert(
+        sql="COPY users_data FROM stdin WITH DELIMITER as ','",
+        filename= file_path
+    )
 
 def _process_user(ti):
     user = ti.xcom_pull(task_ids="user_data") 
@@ -25,7 +40,7 @@ def _process_user(ti):
 with DAG ('user_information', schedule_interval = '@daily', start_date = datetime(2025,1,1), catchup = False) as dag:
     
     create_table= PostgresOperator(
-        task_id = 'create_table',
+        task_id = 'create_table_users',
         postgres_conn_id = 'postgres',
         sql= '''
             CREATE TABLE IF NOT EXISTS users_data (
@@ -40,7 +55,7 @@ with DAG ('user_information', schedule_interval = '@daily', start_date = datetim
 
     )
 
-    check_api_availaibility = HttpSensor(
+    is_api_availaible = HttpSensor(
         task_id = 'is_api_availaible',
         http_conn_id = 'httpconn',
         endpoint = 'api/'
@@ -62,6 +77,12 @@ with DAG ('user_information', schedule_interval = '@daily', start_date = datetim
 
     )
 
-    user_data >> process_user
+    store_user = PythonOperator(
+        task_id = 'store_user',
+        python_callable = _store_user
 
-    
+    )
+
+
+    create_table >> is_api_availaible>> user_data >> process_user >> store_user
+
